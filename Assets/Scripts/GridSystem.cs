71dfbca612ff20b2c705e6e5979c3bbe13b54cf3
Ross Baumgartner <ross.baumgartner@rogers.com>
2021-02-08 00:21:30 -0500

    Initial Grid System Script

---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+    private int width;
+    private int height;
+    private int[,] gridArray;
+    
+    public GridSystem(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+
+        gridArray = new int[width, height];
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
