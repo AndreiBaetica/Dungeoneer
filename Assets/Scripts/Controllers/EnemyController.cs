a2a3dace4604d36f847ebbaa0d43359317fc0bdb
AndreiBaetica <andrei.baetic@gmail.com>
2021-03-18 01:46:42 -0400

    added stubs for behavioural states

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -6,10 +6,17 @@ using UnityEngine.AI;
 
 public class EnemyController : MovementController
 {
-    [SerializeField] private float speed = 10f;
-
     private float lookRadius = 4.5f;
-    Transform target;
+    private Transform target;
+    
+    private BehaviourState currentBehaviourState = BehaviourState.idle;
+
+    private enum BehaviourState
+    {
+        idle,
+        chase,
+        attack
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,31 +28,79 @@ public class EnemyController : MovementController
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(target.position, transform.position);
+        //We absolutely can't have these methods being called once per frame.
+        //Leaving them here until we have a timer system. Then we will only call them once per turn.
+        stateSwitch();
 
-        if (distance <= lookRadius )
+        if (currentBehaviourState == BehaviourState.idle)
         {
-            if (moving) snapToGridSquare();
+            //Idle behaviour
+        } else if (currentBehaviourState == BehaviourState.chase)
+        {
+            //Chase behaviour
 
-            if (Math.Round(transform.position.z) < Math.Round(target.position.z))
-            { //Player is above
-                moveUp();
-            }
-             if (Math.Round(transform.position.x) < Math.Round(target.position.x))
-            { //Player is to the right
-                moveRight();
-            }
-             if (Math.Round(transform.position.z) > Math.Round(target.position.z))
-            { //Player is below
-                moveDown();
+            float distance = Vector3.Distance(target.position, transform.position);
 
-            } if (Math.Round(transform.position.x) > Math.Round(target.position.x))
-            { //Player is to the left
-                moveLeft();
+            if (distance <= lookRadius)
+            {
+                if (moving) snapToGridSquare();
+
+                if (Math.Round(transform.position.z) < Math.Round(target.position.z))
+                {
+                    //Player is above
+                    moveUp();
+                }
+
+                if (Math.Round(transform.position.x) < Math.Round(target.position.x))
+                {
+                    //Player is to the right
+                    moveRight();
+                }
+
+                if (Math.Round(transform.position.z) > Math.Round(target.position.z))
+                {
+                    //Player is below
+                    moveDown();
+
+                }
+
+                if (Math.Round(transform.position.x) > Math.Round(target.position.x))
+                {
+                    //Player is to the left
+                    moveLeft();
+                }
             }
+        } else if (currentBehaviourState == BehaviourState.attack)
+        {
+            //attack behaviour
         }
     }
 
-
+    //stub, fill out the commented if statements with real logic
+    private void stateSwitch()
+    {
+        if (currentBehaviourState == BehaviourState.idle)
+        {
+            /*if (distance to player <= lookRadius)
+            {
+                currentBehaviourState = BehaviourState.chase;
+            }*/
+        } else if (currentBehaviourState == BehaviourState.chase)
+        {
+            /*if (distance to player > lookRadius)
+            {
+                currentBehaviourState = BehaviourState.idle;
+            } else if (distance to player <= 0.5f)
+            {
+                currentBehaviourState = BehaviourState.attack;
+            }*/
+        } else if (currentBehaviourState == BehaviourState.attack)
+        {
+            /*if (distance to player > 0.5f)
+            {
+                currentBehaviourState = BehaviourState.chase;
+            }*/
+        }
+    }
 
 }
