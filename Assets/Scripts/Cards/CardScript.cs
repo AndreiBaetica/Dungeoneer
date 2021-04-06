0bf25d8311254a6bdb23fe1963adb73a2695b88d
Sébastien Gorman <sgorm090@uottawa.ca>
2021-04-06 14:06:39 -0400

    Added simple card UI
    This code is enough to have 1 card work. It does not implement having multiple cards yet or being able to quickly create cards like I did with the items yet. However, I feel pretty confident about being able to reuse some of the thoughts processes that are present in the items creation and the bag button for that.

---
+++ Assets/Scripts/Cards/CardScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour, IPointerClickHandler
+{
+    private Card card;
+
+    [SerializeField] private Sprite visible, notVisible;
+    
+    public Card MyCard
+    {
+        get => card;
+        set
+        {
+            if (value != null)
+            {
+                GetComponent<Image>().sprite = visible; // TODO: change this to full if the player starts with the bag closed instead of open
+            }
+            /*else
+            {
+                GetComponent<Image>().sprite = notVisible;
+            }*/
+            card = value;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // When the card is clicked
+    {
+        //TODO: ROSS: CALL CARD ABILITY FUNCTION HERE
+        updateSprite();
+    }
+    
+    public void updateSprite()
+    {
+        GetComponent<Image>().sprite = notVisible;
+    }
+}
