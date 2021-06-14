193635d86cc80807cedc047d41b3cbdb126a72d5
Carlos Hernandez <chern042@uottawa.ca>
2021-06-13 20:55:33 -0400

    Added Controller For Game Over Screen
    Allows player to be redirected to the game again if they click "Restart", or exit the game if they click "Exit"

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+    {
+        public Text pointsText;
+
+        public void Setup(int score)
+        {
+            gameObject.SetActive(true);
+            pointsText.text = score.ToString() + " POINTS";
+        }
+
+        public void RestartButton()
+        {
+            SceneManager.LoadScene("Scenes/SampleScene");
+        }
+
+        public void ExitButton()
+        {
+            SceneManager.LoadScene("Scenes/Home");
+        }
+    }
