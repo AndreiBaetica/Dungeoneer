d094aaa61f621f9e738176f83a7720cfd26ac607
Sébastien Gorman <sgorm090@uottawa.ca>
2021-11-30 22:21:13 -0500

    added interactable altar prefab
    does not spawn automatically or switch to another dungeon yet.

---
+++ Assets/Scripts/LevelGenerator/Altar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("Player found the altar!");
+        StopInteract();
+    }
+
+    public void StopInteract()
+    {
+        Debug.Log("Generating next floor...");
+    }
+}
