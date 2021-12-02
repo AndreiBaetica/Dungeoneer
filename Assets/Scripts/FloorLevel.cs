03eef7e1c4bd245da647353c37854230aba846a0
Sébastien Gorman <sgorm090@uottawa.ca>
2021-12-02 00:42:37 -0500

    added floor indicator and increased enemy spawn rate

---
+++ Assets/Scripts/FloorLevel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorLevel : MonoBehaviour
+{
+    #region Singleton
+
+    public static FloorLevel instance;
+    
+    void Awake()   
+    {
+        instance = this;
+    }
+        
+    #endregion
+    
+    public static FloorLevel MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<FloorLevel>();
+            }
+
+            return instance;
+        }
+    }
+    
+    public static Text[] pointsText;
+
+    public void UpdateFloorText()
+    {
+        pointsText = FindObjectsOfType<Text>();
+        for (int i = 0; i < pointsText.Length; i++)
+        {
+            if (pointsText[i].name == "FloorText")
+            {
+                pointsText[i].text = "FLOOR "+ UIManager.FinalRoomScore.ToString();
+            }
+        }
+    }
+}
